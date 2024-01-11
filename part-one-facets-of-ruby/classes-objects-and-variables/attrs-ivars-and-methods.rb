b839d1991850f0ef0b544d36c1cd7fa1c03ec29e
Edwin Garcia <egarciavalle2014@gmail.com>
2024-01-10 23:12:13 -0600

    chore: add excerpt from author about attrs-ivars-methods
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

---
+++ part-one-facets-of-ruby/classes-objects-and-variables/attrs-ivars-and-methods.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+=begin
+
+ In this section the author expresses his thoughts
+ about this idea of methods as attributes, in which
+ attributes are technically methods, and when those
+ methods are used as getters/setters, they are seen
+ as logical attributes.
+
+ Adopting this perspective helps people to diferentiate
+ what set of methods are related the state of the object,
+ and what methods are not.
+
+=end
